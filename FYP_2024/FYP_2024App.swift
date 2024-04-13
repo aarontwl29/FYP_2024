a0258f23ba8f5bb46aa62c3ded4c72d1336c33dc
Aaron <220202376@stu.vtc.edu.hk>
2024-04-14 01:03:34 +0800

    Initial Commit

---
+++ FYP_2024/FYP_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  FYP_2024App.swift
+//  FYP_2024
+//
+//  Created by Aaron Tso  on 14/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct FYP_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
