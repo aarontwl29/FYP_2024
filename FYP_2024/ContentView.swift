561eb85858ce63ddd0fc4714a8b64a2d55a675e1
Hi, I'm Tony <tony030824@gmail.com>
2024-04-21 01:18:24 +0800

    Updated

--- FYP_2024/ContentView.swift
+++ FYP_2024/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-                    NavigationLink(destination: TapsView()) {
+                    NavigationLink(destination: MapView()) {
                         Text("Go to Map View")
                     }
                 }
