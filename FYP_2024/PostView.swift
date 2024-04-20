61727b3227395e121a840fa9121eaaee521073da
Hi, I'm Tony <tony030824@gmail.com>
2024-04-21 00:05:00 +0800

    Updated Post View

---
+++ FYP_2024/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  FYP_2024
+//
+//  Created by itst on 20/4/2024.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
