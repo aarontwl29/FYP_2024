17f05262e396acc8532cc0272ae9f6ebdb8ab5cf
Aaron <220202376@stu.vtc.edu.hk>
2024-04-20 15:40:50 +0800

    .

--- FYP_2024/MapView.swift
+++ FYP_2024/MapView.swift
@@ -1,10 +1,17 @@
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 22.3080, longitude: 113.9185),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+
     var body: some View {
-        Text("Hello World")
-            .font(.largeTitle)
-            .padding()
+        VStack {
+                    Map_SearchBar()
+                    Map(coordinateRegion: $region)
+                }
     }
 }
 
