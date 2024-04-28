591aa65e119d8e585854d96bd5eceace7c383065
Aaron <220202376@stu.vtc.edu.hk>
2024-04-28 21:10:00 +0800

    submit successfully

--- FYP_2024/FYP_2024App.swift
+++ FYP_2024/FYP_2024App.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct FYP_2024App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            ReportingView()
         }
     }
 }
--- FYP_2024/Report.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Report.swift
-//  FYP_2024
-//
-//  Created by Aaron Tso  on 28/4/2024.
-//
-
-import Foundation
