68e5579d862984df3236cb57064176354046047c
Aaron <220202376@stu.vtc.edu.hk>
2024-04-29 23:48:03 +0800

    delete sucess

--- FYP_2024/MyPostDetailView.swift
+++ FYP_2024/MyPostDetailView.swift
@@ -50,7 +50,8 @@ struct MyPostDetailsView: View {
                     
                     ScrollView {
                         Button(action: {
-                            // 實作按鈕的功能
+                            print("Delete button tapped")
+                            deleteReport()
                         }) {
                             InfoBubble(buttonInfo: "Delete")
                         }
@@ -75,123 +76,150 @@ struct MyPostDetailsView: View {
         return formatter.string(from: date)
     }
     
+    func deleteReport() {
+        print("Attempting to delete report")
+        guard let url = URL(string: "https://fyp2024.azurewebsites.net/reports/\(report.report.reportId)") else {
+            print("Invalid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("Error: Invalid response or status code")
+                return
+            }
+            print("Report deleted successfully")
+            
+            DispatchQueue.main.async {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }.resume()
+    }
+    
+    
 }
-    
-    
-    struct InfoBubble: View {
-        var buttonInfo: String
-        var body: some View {
-            VStack {
-                Button(action: {
-                    // 寫上導航到其他頁面的程式碼
-                }) {
-                    Text(buttonInfo)
-                        .foregroundStyle(.red).bold()
-                }
-                .frame(width:290 , height: 50)
-                .background(Color.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray, lineWidth: 1)
-                )
+
+
+struct InfoBubble: View {
+    var buttonInfo: String
+    var body: some View {
+        VStack {
+            Button(action: {
+                // 寫上導航到其他頁面的程式碼
+            }) {
+                Text(buttonInfo)
+                    .foregroundStyle(.red).bold()
             }
-            .padding()
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .cornerRadius(10)
-            .shadow(radius: 1)
-        }
-    }
-    
-    
-    
-    struct DetailBubble: View {
-        var title: String
-        var detail: String
-        
-        var body: some View {
-            VStack {
-                Text(title)
-                    .font(.footnote)
-                    .foregroundColor(.blue)
-                Text(detail)
-                    .font(.headline)
-                    .foregroundColor(.gray)
-            }
-            .padding()
-            .frame(minWidth: 0, maxWidth: .infinity)
+            .frame(width:290 , height: 50)
             .background(Color.white)
-            .cornerRadius(10)
-            .shadow(radius: 1)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
         }
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .cornerRadius(10)
+        .shadow(radius: 1)
+    }
+}
+
+
+
+struct DetailBubble: View {
+    var title: String
+    var detail: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.footnote)
+                .foregroundColor(.blue)
+            Text(detail)
+                .font(.headline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 1)
+    }
+}
+
+struct ReportInfo {
+    var location: String
+    var appearTime: String
+    var disappearTime: String
+    var date: String
+}
+
+
+struct CameraInfoGridView: View {
+    
+    let reportInfo: [ReportInfo]
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 20),
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
+    func truncatedLocation(_ location: String) -> String {
+        let maxLength = 30
+        return location.count > maxLength ? String(location.prefix(maxLength)) + "..." : location
     }
     
-    struct ReportInfo {
-        var location: String
-        var appearTime: String
-        var disappearTime: String
-        var date: String
-    }
-    
-    
-    struct CameraInfoGridView: View {
-        
-        let reportInfo: [ReportInfo]
-        
-        let columns: [GridItem] = [
-            GridItem(.flexible(), spacing: 20),
-            GridItem(.flexible(), spacing: 10),
-            GridItem(.flexible(), spacing: 10),
-            GridItem(.flexible(), spacing: 0)
-        ]
-        
-        func truncatedLocation(_ location: String) -> String {
-            let maxLength = 30
-            return location.count > maxLength ? String(location.prefix(maxLength)) + "..." : location
-        }
-        
-        var body: some View {
-            ScrollView {
-                LazyVGrid(columns: columns, alignment: .leading, spacing: 20) {
-                    Text("Location").bold().foregroundStyle(.blue)
-                    Text("Appear").bold().foregroundStyle(.blue)
-                    Text("Dissipate").bold().foregroundStyle(.blue)
-                    Text("Date").bold().foregroundStyle(.blue)
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, alignment: .leading, spacing: 20) {
+                Text("Location").bold().foregroundStyle(.blue)
+                Text("Appear").bold().foregroundStyle(.blue)
+                Text("Dissipate").bold().foregroundStyle(.blue)
+                Text("Date").bold().foregroundStyle(.blue)
+                
+                ForEach(reportInfo, id: \.location) { info in
                     
-                    ForEach(reportInfo, id: \.location) { info in
-                        
-                        Text(self.truncatedLocation(info.location)).foregroundStyle(.black)
-        
-                        Text(self.truncatedLocation(info.appearTime)).foregroundStyle(.black)
-                        
-                        Text(self.truncatedLocation(info.disappearTime)).foregroundStyle(.black)
-                        
-                        Text(self.truncatedLocation(info.date)).foregroundStyle(.black)
-                    }
+                    Text(self.truncatedLocation(info.location)).foregroundStyle(.black)
+                    
+                    Text(self.truncatedLocation(info.appearTime)).foregroundStyle(.black)
+                    
+                    Text(self.truncatedLocation(info.disappearTime)).foregroundStyle(.black)
+                    
+                    Text(self.truncatedLocation(info.date)).foregroundStyle(.black)
                 }
-                .padding()
             }
-            .background(Color(.systemGroupedBackground))
+            .padding()
         }
-        
+        .background(Color(.systemGroupedBackground))
     }
-    
-    
-    // 定義流浪動物數據結構體
-    struct StrayAnimal {
-        var imageName: String
-        var breed: String
-        var colors: String
-        var gender: String
-        var size: String
-        var address: String
-        var date: String
-    }
-    
-    struct StrayInfoDetal {
-        var age: String
-        var sex: String
-        var species: String
-        var color: String
-        var neutered: String
-        var health: String
-    }
+}
+
+
+// 定義流浪動物數據結構體
+struct StrayAnimal {
+    var imageName: String
+    var breed: String
+    var colors: String
+    var gender: String
+    var size: String
+    var address: String
+    var date: String
+}
+
+struct StrayInfoDetal {
+    var age: String
+    var sex: String
+    var species: String
+    var color: String
+    var neutered: String
+    var health: String
+}
